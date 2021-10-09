7e74a85d6f051bec3e76cabe61482a64d613c3d5
JamesPark89 <87226158+JamesPark89@users.noreply.github.com>
2021-10-09 16:42:38 +1100

    initialize project
    initialize project

---
+++ private/header.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+  <link  rel="stylesheet" href="<?php echo url_for('css/style.css');?>">
+  <title>Document</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="<?php echo url_for('index.php');?>">AusLife</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="<?php echo url_for('house/index.php');?>">House</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo url_for('/job/index.php');?>">Job</a>
+        </li>
+        <li class="nav-item me-auto">
+          <a class="nav-link" href="">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ public/job/index.php
@@ -0,0 +1,5 @@
+<?php include_once('../../private/initialize.php')?>
+
+<?php include_once(PRIVATE_PATH .'/header.php')?>
+
+<?php include_once(PRIVATE_PATH .'/footer.php')?>
