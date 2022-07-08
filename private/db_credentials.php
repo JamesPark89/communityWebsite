2ec486f728be30b29561dd3c816c6dc950d12c62
James Park <87226158+JamesPark89@users.noreply.github.com>
2022-07-08 15:00:25 +1000

    add .htaccess
    add .htaccess

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,8 +1,13 @@
 <?php
+// heroku db
+   define("DB_SERVER", "us-cdbr-east-06.cleardb.net");
+   define("DB_USER", "b4ae1fd6a097e7");
+   define("DB_PASS", "17404603");
+   define("DB_NAME", "communityweb");
 
-  define("DB_SERVER", "localhost");
-  define("DB_USER", "webuser");
-  define("DB_PASS", "xornjs12");
-  define("DB_NAME", "communityweb");
+  // define("DB_SERVER", "localhost");
+  // define("DB_USER", "webuser");
+  // define("DB_PASS", "xornjs12");
+  // define("DB_NAME", "communityweb");
 
 ?>
