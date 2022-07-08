5371e92d78e24c8516d6d928afa98533f9a74623
James Park <87226158+JamesPark89@users.noreply.github.com>
2022-07-08 15:10:58 +1000

    update .htaccess
    update .htaccess

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -3,7 +3,7 @@
    define("DB_SERVER", "us-cdbr-east-06.cleardb.net");
    define("DB_USER", "b4ae1fd6a097e7");
    define("DB_PASS", "17404603");
-   define("DB_NAME", "communityweb");
+   define("DB_NAME", "");
 
   // define("DB_SERVER", "localhost");
   // define("DB_USER", "webuser");
